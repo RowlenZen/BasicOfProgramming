e7e45f70326725c5556b669e0d0b53342c505efe
Anton Makarov <makarovtonyy@gmail.com>
2022-02-16 03:27:22 +0300

    fun sortRowsByMaxElement

--- libs/data_structures/matrix/test-tasks.c
+++ libs/data_structures/matrix/test-tasks.c
@@ -28,7 +28,44 @@ void swapRowsWithMaxAndMinValues_test() {
     );
 
     swapRowsWithMaxAndMinValues_task(testMatrix);
+    assert(areTwoMatricesEqual(testMatrix, endMatrix));
 
+    freeMemMatrix(testMatrix);
+    freeMemMatrix(endMatrix);
+}
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 0; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
+
+/**Упорядочить строки матрицы по неубыванию наибольших элементов строк*/
+void sortRowsByMaxElement_task(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
+void sortRowsByMaxElement_test(){
+    matrix testMatrix = createMatrixFromArray(
+            (int[]) {
+                    0, 7, 14,
+                    1, 56, 52,
+                    32, 4, 2
+            },
+            3, 3);
+
+    matrix endMatrix = createMatrixFromArray(
+            (int[]) {
+                    0, 7, 14,
+                    32, 4, 2,
+                    1, 56, 52
+            }, 3, 3
+    );
+
+    sortRowsByMaxElement_task(testMatrix);
     assert(areTwoMatricesEqual(testMatrix, endMatrix));
 
     freeMemMatrix(testMatrix);
@@ -37,4 +74,6 @@ void swapRowsWithMaxAndMinValues_test() {
 
 void matrix_test() {
     swapRowsWithMaxAndMinValues_test();
+    sortRowsByMaxElement_test();
+//    sortColsByMinElement_test();
 }
